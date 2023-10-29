aeab5db8c260eede048a0c01b9216fa2bf8d448f
Alina <64754849+kvashenaya@users.noreply.github.com>
2023-10-29 04:38:31 +0200

    Delete dist directory

--- dist/posts/controllers/post.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { PostDto } from 'src/posts/dto/post.dto';
-import { PostsService } from 'src/posts/services/post.service';
-export declare class PostsController {
-    private readonly postService;
-    constructor(postService: PostsService);
-    getPosts(): Promise<import("../../typeorm").Post[]>;
-    findPostsById(id: number): Promise<import("../../typeorm").Post>;
-    createPosts(createPostDto: PostDto): Promise<import("../../typeorm").Post>;
-}
--- dist/typeorm/post.entity.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Post {
-    id: number;
-    firstname: string;
-    lastname: string;
-    email: string;
-    telephone: string;
-}
